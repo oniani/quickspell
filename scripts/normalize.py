ace8e95631d38a73ab0e96310d8b6d077824f88a
David Oniani <onianidavid@gmail.com>
2019-04-25 20:19:08 -0500

    Updated the dictionary and modified the Python script

--- scripts/normalize.py
+++ scripts/normalize.py
@@ -5,15 +5,17 @@ and convert all the words to the UTF-8 format.
 
 
 def main():
-	with open('../data/large.words', 'r') as file:
-		lines = sorted(set(map(lambda x: x.lower(), file.readlines())))
+    with open('../data/large.words', 'r') as file:
+        # I know, this is not "Pythonic," but I do not care (:
+        lines = sorted(set(filter(lambda x: x.strip('\n').isalpha(),
+                       map(lambda x: x.lower(), file.readlines()))))
 
-	with open('../data/normalized.words', 'w') as new_file:
-		for line in lines:
-			if '\'' not in line:
-				line = bytes(line, 'utf-8').decode('utf-8', 'ignore')
-				new_file.write(line)
+    with open('../data/normalized.words', 'w') as new_file:
+        for line in lines:
+            if '\'' not in line:
+                line = bytes(line, 'utf-8').decode('utf-8', 'ignore')
+                new_file.write(line)
 
 
 if __name__ == "__main__":
-	main()
+    main()
